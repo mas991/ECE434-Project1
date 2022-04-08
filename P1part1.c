fa2bde0364e3a6a76249ab042a82005ecfc57a13
mas991 <94929415+mas991@users.noreply.github.com>
2022-04-07 21:37:29 -0400

    Minor Edits

--- P1part1.c
+++ P1part1.c
@@ -1,68 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
 
-int L; // Command Line Argument 1
-int H; // Command Line Argument 2
-FILE * filePtrIn; // Input File Pointer
-FILE * filePtrOut; // Output File Pointer
+int L;
+int H;
+FILE * filePtrIn;
+FILE * filePtrOut;
 
 int main(int argc, char *argv[]) {
 
-        filePtrIn = fopen("P1Input.txt","r"); // Open file called "P1Input.txt" for reading
-        if(filePtrIn == NULL) { // Error
+        struct timeval start, end;
+        gettimeofday(&start,NULL);
+
+        filePtrIn = fopen("P1Input.txt","r");
+        if(filePtrIn == NULL) {
                 printf("Error: File open failed.\n");
                 exit(1);
         }
 
-        filePtrOut = fopen("P1Part1.txt","w"); // Open file called "P1Part1.txt" for writing
-        if(filePtrOut == NULL) { // Error
+        filePtrOut = fopen("P1Part1.txt","w");
+        if(filePtrOut == NULL) {
                 printf("Error: File open failed.\n");
                 exit(1);
         }
 
-        fprintf(filePtrOut,"Hi I'm process %d and my parent is %d.\n",getpid(),getppid()); // Print Process information
+        fprintf(filePtrOut,"Hi I'm process %d and my parent is %d.\n",getpid(),getppid());
         L = atoi(argv[1]);
         H = atoi(argv[2]);
 
-        if(L < 3000) { // L out of bounds
+        if(L < 3000) {
                 fprintf(filePtrOut,"Error: Argument L should be greater than or equal to 3000.\n");
                 exit(1);
         }
-        if(H <= 0 || H > 10) { // H out of bounds
+        if(H <= 0 || H > 10) {
                 fprintf(filePtrOut,"Error: Argument H should be greater than 0 and less than or equal to 10.\n");
                 exit(1);
         }
-
-        int *numList = (int*) malloc(L*sizeof(int)); // Array to hold input list of integers
-        for(int i=0; i<L; i++) {
-                fscanf(filePtrIn,"%d",&numList[i]); // Scan input file to populate array
-        }
         
-        int maxNum = 0; // Maximum recorded integer
-        int sum = 0; // Sum of all integers
-        int * keyLoc = (int*) malloc(H*sizeof(int)); // Location of hidden keys
-        int currKey = 0; // Index for keyLoc
+        int *numList = (int*) malloc(L*sizeof(int));
+        for(int i=0; i<L; i++) {
+                fscanf(filePtrIn,"%d",&numList[i]);
+        }
+
+        int maxNum = 0;
+        float avg = 0;
+        int * keyLoc = (int*) malloc(H*sizeof(int));
+        int currKey = 0;
 
         for(int i=0;i<L;i++) {
                 if(numList[i] > maxNum) {
-                        maxNum = numList[i]; // Update maxNum
+                        maxNum = numList[i];
                 }
-                sum += numList[i]; // Update sum
-                if(numList[i] == -1) { // Update keyLoc, currKey
+                avg += numList[i]/((float)L);
+                if(numList[i] == -1) {
                         keyLoc[currKey] = i;
                         currKey++;
                 }
+                if(currKey == H) {
+                        break;
+                }
         }
-        float avg = ((float)sum / (float)L); // Calculate Average
 
-        fprintf(filePtrOut,"Max = %d, Avg = %f.\n\n",maxNum,avg); // Print Input file information
-        for(int i=0;i<H;i++) { // Print key locations and who found each key
+        fprintf(filePtrOut,"Max = %d, Avg = %f.\n\n",maxNum,avg);
+        for(int i=0;i<H;i++) {
                 fprintf(filePtrOut,"Hi I am process %d and I found the hidden key in position A[%d].\n",getpid(),keyLoc[i]);
         }
-        
-        free(numList); // Free memory
+
+        free(numList);
         free(keyLoc);
-        
+
+        gettimeofday(&end,NULL);
+        float runTime = (end.tv_usec - start.tv_usec)/1000000.0;
+
+        printf("Time Elapsed: %f.\n",runTime);
+
         return 0;
 }
